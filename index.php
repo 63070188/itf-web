ba9c62a6199fe2f50f36e295610500560660072c
63070188 <68292431+63070188@users.noreply.github.com>
2020-10-30 15:40:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,34 @@
-<?php
-echo "<h2>IT@KMITL</h2>";
-echo "Hello IT<br>";
-?>
+<html>
+    <head>
+        <metra charset="UTF-8">
+        <title>Hello world</title>
+    </head>
+    <body>
+        <h1>Beautyful Land</h1>
+        <p>Name: Aphatsara Moratsathian</p>
+        <p>Nickname: Por</p>
+        <img src="beauty.jpg">
+        <br>
+        <a href="https://www.wongnai.com/beauty-tips/makeup-eyes">Go To Link!</a>
+
+        <h2>พาเลตต์อายแชโดว์ที่ต้องมี!!</h2>
+        <ul>
+            <li>Etude House</li>
+            <li>Peripera</li>
+            <li>Holika Holika</li>
+        </ul>
+        <h3> "เรื่องตลกที่เหมือนจะตลก" </h3>
+        พ่อ : คราวหน้าถ้าสอบตกอีกไม่ต้องมาเรียกว่า พ่อ<br>
+        ลูก : ครับ<br>
+        -----ผลการสอบออก-----<br>
+        พ่อ : ผลสอบเป็นไงบ้างลูก<br>
+        ลูก : สวัสดีครับ ใครลูกคุณ :)<br><br>
+        <img src="fah.jpg">
+        <h4> "จีบวันละนิดจิตแจ่มใส" </h4>
+        A : ดูท้องฟ้าสิ เล็กจัง<br>
+        B : ทำไมหรอ<br>
+        A : เขียนคำว่า"รักเธอ"ยังไม่พอ<br>
+        #########################<br>
+
+    </body>
+</html>
